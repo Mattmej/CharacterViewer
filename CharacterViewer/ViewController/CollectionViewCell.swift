d765f15be1246030d6b2a97144f249c21adbbf50
Matt Mejia <matt@ADMINs-MacBook-Pro.local>
2018-12-19 22:18:05 -0500

    Add collection view cell to collection view.

---
+++ CharacterViewer/ViewController/CollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionViewCell.swift
+//  CharacterViewer
+//
+//  Created by Matt Mejia on 12/19/18.
+//  Copyright © 2018 Matt_Mejia. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
