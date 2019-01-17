78f0b6c5bcf53802f28e26bd8761b6835f9d55b5
Mattmej <mattcmej@gmail.com>
2019-01-16 19:42:07 -0500

    Begin writing ServiceManager.

---
+++ CharacterViewer/Manager/ServiceManager.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceManager.swift
+//  CharacterViewer
+//
+//  Created by Matt Mejia on 1/16/19.
+//  Copyright © 2019 Matt_Mejia. All rights reserved.
+//
+
+import Foundation
+
+class ServiceManager {
+    
+    // our url
+    // first, we will retrieve all characters.
+    static let url = URL(string: "https://api.duckduckgo.com/?q=looney+tunes+characters&format=json")
+    
+//    static func getCharacters(completion: @escaping(c))
+}
